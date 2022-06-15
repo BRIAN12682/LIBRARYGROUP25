434f2274711a34793ec7fd7d541ca4dc415cc3de
AYT-BRAIN <atbrian19@gmail.com>
2022-06-15 19:21:58 +0300

    user authetication page for the admin and the user-Build2 modified

--- Librarysystems/my_app/models.py
+++ Librarysystems/my_app/models.py
@@ -26,6 +26,7 @@ class Books(models.Model):
     def __str__(self):
         return self.title
 
+
 '''    def delete(self, *args, **kwargs ):
         self.pdf.delete()
         self.cover.delete()
