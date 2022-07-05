0a37b2e42627419061cf83a9fe0c9539450a6a68
Richardmugi <rmugisha927@gmail.com>
2022-07-05 04:25:45 -0700

    request functions

---
+++ userinterface/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+def login(request):
+    return(request,'login.html')
+
+def register(request):
+    return(request,'register.html')
+
+def index(request):
+    return(request,'index.html')
+def home(request):
+    return(request,'home.html')
+
+
+
+
+
+
+
+
+
