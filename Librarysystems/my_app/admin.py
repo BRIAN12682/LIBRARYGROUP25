18e42017bd00744a44fe3f9a27230dd1ff72b3b1
AYT-BRAIN <atbrian19@gmail.com>
2022-07-18 19:46:45 +0300

    Initial pages, others to be uploaded tomorrow

--- Librarysystems/my_app/admin.py
+++ Librarysystems/my_app/admin.py
@@ -1,36 +1,5 @@
-'''from django.contrib import admin
-from django.contrib.auth.models import a
-from django.contrib.auth.models import models
-
-# Create your models here.
-
-
-class Admin(admin):
-    is_admin = models.BooleanField(default=False)
-    is_publisher = models.BooleanField(default=False)
-    is_librarian = models.BooleanField(default=False)
-
-    class Meta:
-        swappable = 'AUTH_USER MODEL'
-
-
-class Books(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    year = models.CharField(max_length=100)
-    publisher = models.CharField(max_length=100)
-    description = models.CharField(max_length=255)
-    pdf = models.FileField(upload_to='uploads/pdfs/', null=True, blank=True)
-    cover = models.ImageField(upload_to='uploads/cover/', null=True, blank=True)
-
-    def __str__(self):
-        return self.title
-
-'''
-'''    def delete(self, *args, **kwargs ):
-        self.pdf.delete()
-        self.cover.delete()
-        super().delete(*args, **kwargs)
-'''
-
+from django.contrib import admin
+from .models import Book
 # Register your models here.
+
+admin.site.register(Book)
