a1db9a071c356fbd4643d20132486895f3d5b697
AYT-BRAIN <atbrian19@gmail.com>
2022-07-25 20:02:04 +0300

    heroku hosting update 2

--- NEWLIBSYSTEM/LIB25/LIB25/settings.py
+++ NEWLIBSYSTEM/LIB25/LIB25/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
-import os
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'LIB25',
     'lib',
 ]
 
