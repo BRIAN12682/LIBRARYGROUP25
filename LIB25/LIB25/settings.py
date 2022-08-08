dc69c202fa458acc93f115a286325bbea8712ece
AYT-BRAIN <atbrian19@gmail.com>
2022-08-08 20:14:40 +0300

    tweaked app for heroku

--- LIB25/LIB25/settings.py
+++ LIB25/LIB25/settings.py
@@ -15,6 +15,9 @@ import django_heroku
 import dj_database_url
 import os
 from decouple import config
+import django_heroku
+import dj_database_url
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
