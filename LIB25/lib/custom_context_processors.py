469ed7e0e8d2cf3744a970e08cbb1c9f27f87636
AYT-BRAIN <atbrian19@gmail.com>
2022-08-26 15:20:30 +0300

    LIB25

--- LIB25/lib/custom_context_processors.py
+++
@@ -1,4 +0,0 @@
-from notifications_app.models import BroadcastNotification
-def notifications(request):
-    allnotifications = BroadcastNotification.objects.all()
-    return {'notifications': allnotifications}
